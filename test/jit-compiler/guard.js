3e3ca7d2d1c77393656b8e9a0d520fdb5c939de0
Falco Nogatz <fnogatz@gmail.com>
2015-08-08 04:20:28 +0200

    Add guard tests for usage with user-defined predicates

--- test/jit-compiler/guard.js
+++ test/jit-compiler/guard.js
@@ -41,3 +41,33 @@ test('a ==> ${ () => 1 > 2 } | b', function (t) {
 
   t.end()
 })
+
+test('Scope', function (t) {
+  var chr = new CHR()
+  chr('a(N) ==>', function (N) { return N > 10 }, '| b')
+
+  chr.a(1)
+  t.equal(chr.Store.length, 1)
+
+  chr.a(20)
+  t.equal(chr.Store.length, 3)
+
+  t.end()
+})
+
+test('Guard with used-defined predicate', function (t) {
+  function greaterThan10 (N) {
+    return N > 10
+  }
+
+  var chr = new CHR()
+  chr('a(N) ==>', greaterThan10, '| b')
+
+  chr.a(1)
+  t.equal(chr.Store.length, 1)
+
+  chr.a(20)
+  t.equal(chr.Store.length, 3)
+
+  t.end()
+})
