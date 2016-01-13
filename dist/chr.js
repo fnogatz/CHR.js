a35c8e80ca08bd28ae877c7533408bc3184b5cc0
Falco Nogatz <fnogatz@gmail.com>
2016-01-13 11:02:06 +0100

    dist for v2.0.5

--- dist/chr.js
+++ dist/chr.js
@@ -2518,7 +2518,7 @@ function hash (ids) {
   CHR.History = Runtime.History
   CHR.Rule = Rule
 
-  CHR.version = '2.0.4'
+  CHR.version = '2.0.5'
 
   CHR.noConflict = function () {
     root.CHR = prevCHR
