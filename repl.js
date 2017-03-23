eba03d1590d247caddd604e75dbf516e69f4311f
Falco Nogatz <fnogatz@gmail.com>
2017-03-23 01:13:19 +0100

    Add introspection commands to REPL

--- repl.js
+++ repl.js
@@ -13,13 +13,78 @@ function Repl () {
     input: process.stdin,
     output: process.stdout,
     eval: function (cmd, context, filename, callback) {
+      cmd = cmd.trim()
+      var re
+      var ruleName
+
+      // is it a special command?
+      if (cmd === '/exit') {
+        process.exit(0)
+      }
+      if (cmd === '/rules') {
+        chr.Rules.ForEach(function (rule) {
+          r.output.write(require('util').inspect(rule._source, false, null))
+        })
+
+        return callback()
+      }
+      re = /^\/rule (.*)$/
+      if (re.test(cmd)) {
+        // e.g. `/rule rule-name`
+        ruleName = cmd.replace(re, '$1')
+        if (!chr.Rules[ruleName]) {
+          r.output.write('No rule with name ' + ruleName)
+          return callback()
+        }
+
+        r.output.write(require('util').inspect(chr.Rules[ruleName], false, null))
+        return callback()
+      }
+      re = /^\/rule-functor ([^ ]*) (.*)$/
+      if (re.test(cmd)) {
+        // e.g. `/rule-functor rule-name functor-name`
+        ruleName = cmd.replace(re, '$1')
+        var functor = cmd.replace(re, '$2')
+
+        if (!chr.Rules[ruleName]) {
+          r.output.write('No rule with name ' + ruleName)
+          return callback()
+        }
+
+        var occurrence = null
+        re = /^(.*)\[([0-9]+)\]$/
+        if (re.test(cmd)) {
+          // has occurrence number
+          occurrence = functor.replace(re, '$2')
+          functor = functor.replace(re, '$1')
+        }
+
+        if (!chr.Rules[ruleName][functor]) {
+          r.output.write('Rule ' + ruleName + ' has no functor ' + functor)
+          return callback()
+        }
+
+        if (occurrence !== null) {
+          if (!chr.Rules[ruleName][functor][occurrence]) {
+            r.output.write('Rule ' + ruleName + ' has no occurrence ' + occurrence + ' of functor ' + functor)
+            return callback()
+          }
+
+          r.output.write(chr.Rules[ruleName][functor][occurrence].toString())
+          return callback()
+        }
+
+        r.output.write(require('util').inspect(chr.Rules[ruleName][functor], false, null))
+        return callback()
+      }
+
       // try Program
       try {
-        var rules = parse(cmd.trim(), { startRule: 'Program' })
+        var rules = parse(cmd, { startRule: 'Program' })
       } catch (e) {
         // try Query
         try {
-          var queries = parse(cmd.trim(), { startRule: 'Query' })
+          var queries = parse(cmd, { startRule: 'Query' })
         } catch (e) {
           // no query
           var res = eval(cmd) // eslint-disable-line
