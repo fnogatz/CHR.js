aa2aec2b6bc1a2be363dbe7898f42810fb19b08c
Falco Nogatz <fnogatz@gmail.com>
2017-03-24 13:08:50 +0100

    Add more tests for string/int types as constraint arguments or in the guard

--- test/examples/issue-20.js
+++ test/examples/issue-20.js
@@ -2,7 +2,7 @@ var test = require('tape')
 
 var CHR = require('../../src/index')
 
-test("String arguments (Issue #20)", function (t) {
+test('String arguments (Issue #20)', function (t) {
   var chr = new CHR()
   chr("a('p') ==> b")
 
@@ -13,6 +13,74 @@ test("String arguments (Issue #20)", function (t) {
       t.equal(chr.Store.length, 3)
 
       t.end()
+    })
+  })
+})
+
+test('String arguments (Issue #20), with Int value', function (t) {
+  var chr = new CHR()
+  chr("a('42') ==> b")
+
+  chr.a(42).then(function () {
+    t.equal(chr.Store.length, 1)
+
+    chr.a('42').then(function () {
+      t.equal(chr.Store.length, 3)
+
+      t.end()
+    })
+  })
+})
+
+test('Int arguments', function (t) {
+  var chr = new CHR()
+  chr('a(42) ==> b')
+
+  chr.a('q').then(function () {
+    t.equal(chr.Store.length, 1)
+
+    chr.a('42').then(function () {
+      t.equal(chr.Store.length, 2)
+
+      chr.a(42).then(function () {
+        t.equal(chr.Store.length, 4)
+
+        t.end()
+      })
+    })
+  })
+})
+
+test('Check String argument in guard', function (t) {
+  var chr = new CHR()
+  chr("a(X) ==> X === 'p' | b")
+
+  chr.a('q').then(function () {
+    t.equal(chr.Store.length, 1)
+
+    chr.a('p').then(function () {
+      t.equal(chr.Store.length, 3)
+
+      t.end()
+    })
+  })
+})
+
+test('Check String argument in guard, with Int value', function (t) {
+  var chr = new CHR()
+  chr("a(X) ==> X === '42' | b")
+
+  chr.a('q').then(function () {
+    t.equal(chr.Store.length, 1)
+
+    chr.a(42).then(function () {
+      t.equal(chr.Store.length, 2)
+
+      chr.a('42').then(function () {
+        t.equal(chr.Store.length, 4)
+
+        t.end()
+      })
     })
   })
 })
